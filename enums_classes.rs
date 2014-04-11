d6a588efa514f970e6f1549a8b85b526c21b5321
JoshAshby <joshuaashby@joshashby.com>
2014-04-11 12:51:07 -0600

    working on fleshing enums_classes out. hint: beaglebone

--- enums_classes.rs
+++ enums_classes.rs
@@ -1,18 +1,12 @@
 use std::io;
 
 
-fn main() {
-  let f = Pin::new(46);
-  f.set_direction(IN);
-  let res = f.get_direction();
-  println!("Direction: {}", (res as int));
-}
-
 static base_path: &'static str = "";
 
+
 enum Direction {
-  IN,
-  OUT
+  OUT,
+  IN
 }
 
 enum Value {
@@ -22,55 +16,86 @@ enum Value {
 
 
 struct Pin {
-  pin: int,
+  pin: int
 }
 
 impl Pin {
-  fn new(p: int) -> Pin {
+  pub fn new(p: int) -> Pin {
     Pin {
       pin: p
     }
   }
 
-  fn export(&self) {
-    let path = Path::new(format!("{}/{}/export", base_path, self.pin));
-    let mut file = match io::File::open_mode(&path, io::Open, io::Write).write_int(self.pin) {
-      Ok(f) => f,
-      Err(e) => fail!("Can't export pin: {}", e)
-    };
+  fn get_path(&self, ep: &str) -> (~str) {
+    //format!("{}/{}/{}", base_path, self.pin, ep)
+    format!("{}/{}", self.pin, ep)
   }
 
-  fn unexport(&self) {
-    let path = Path::new(format!("{}/{}/unexport", base_path, self.pin));
-    let mut file = match io::File::open_mode(&path, io::Open, io::Write).write_int(self.pin) {
+  pub fn export(&self) {
+    //let path = Path::new(format!("{}/export", base_path));
+    let path = Path::new("export");
+    let mut file = match io::File::open_mode(&path, io::Open, io::Write) {
       Ok(f) => f,
-      Err(e) => fail!("Can't export pin: {}", e)
+      Err(e) => fail!("Can't open export file: {}", e)
     };
+    match file.write_int(self.pin) {
+      Ok(()) => (),
+      Err(e) => fail!("Can't write to export file: {}", e)
+    };
+    match file.fsync() {
+      Ok(()) => (),
+      Err(e) => fail!("Can't sync export file: {}", e)
+    }
   }
 
-  fn set_direction(&self, d: Direction) {
-    let path = Path::new(format!("{}/{}/direction", base_path, self.pin));
+  pub fn unexport(&self) {
+    //let path = Path::new(format!("{}/unexport", base_path));
+    let path = Path::new("unexport");
+    let mut file = match io::File::open_mode(&path, io::Open, io::Write) {
+      Ok(f) => f,
+      Err(e) => fail!("Can't open unexport file: {}", e)
+    };
+    match file.write_int(self.pin) {
+      Ok(()) => (),
+      Err(e) => fail!("Can't write to unexport file: {}", e)
+    };
+    match file.fsync() {
+      Ok(()) => (),
+      Err(e) => fail!("Can't sync unexport file: {}", e)
+    }
+  }
+
+  pub fn set_direction(&self, d: Direction) {
+    let path = Path::new(self.get_path("direction"));
     let a = match d {
       OUT => "out",
       IN => "in"
     };
-    let mut file = match io::File::open_mode(&path, io::Open, io::Write).write_str(a) {
-      Ok(()) => (),
-      Err(e) => fail!("Can't set pin direction: {}", e)
+    let mut file = match io::File::open_mode(&path, io::Open, io::Write) {
+      Ok(f) => f,
+      Err(e) => fail!("Can't open pin direction file: {}", e)
     };
+    match file.write_str(a) {
+      Ok(()) => (),
+      Err(e) => fail!("Can't write to pin direction file: {}", e)
+    };
+    match file.fsync() {
+      Ok(()) => (),
+      Err(e) => fail!("Can't sync pin direction file: {}", e)
+    }
   }
 
-  fn input(&self) {
+  pub fn input(&self) {
     self.set_direction(IN);
   }
 
-  fn output(&self) {
+  pub fn output(&self) {
     self.set_direction(OUT);
   }
 
-  fn get_direction(&self) -> (Direction) {
-    let path = Path::new(format!("{}/{}/direction", base_path, self.pin));
-    let mut file = match io::File::open_mode(&path, io::Open, io::Read).read_to_str() {
+  pub fn get_direction(&self) -> (Direction) {
+    let path = Path::new(self.get_path("direction"));
+    let file = match io::File::open_mode(&path, io::Open, io::Read).read_to_str() {
       Ok(f) => f,
       Err(e) => fail!("Can't read pin direction: {}", e)
     };
@@ -81,11 +106,130 @@ impl Pin {
     }
   }
 
-  fn set_value(&self, v: Value) {
-    let path = Path::new(format!("{}/{}/value", base_path, self.pin));
-    let mut file = match io::File::open_mode(&path, io::Open, io::Write).write_int((v as int)) {
-      Ok(()) => (),
-      Err(e) => fail!("Can't set pin value: {}", e)
+  pub fn set_value(&self, v: Value) {
+    let path = Path::new(self.get_path("value"));
+    let mut file = match io::File::open_mode(&path, io::Open, io::Write) {
+      Ok(f) => f,
+      Err(e) => fail!("Can't open pin value file: {}", e)
     };
+    match file.write_int((v as int)) {
+      Ok(()) => (),
+      Err(e) => fail!("Can't write to pin value file: {}", e)
+    };
+    match file.fsync() {
+      Ok(()) => (),
+      Err(e) => fail!("Can't sync pin value file: {}", e)
+    }
   }
+
+  pub fn high(&self) {
+    self.set_value(HIGH);
+  }
+
+  pub fn low(&self) {
+    self.set_value(LOW);
+  }
+
+  pub fn get_value(&self) -> (Value) {
+    let path = Path::new(self.get_path("value"));
+    let i = match io::File::open_mode(&path, io::Open, io::Read).read_to_str() {
+      Ok(f) => f,
+      Err(e) => fail!("Can't read pin direction: {}", e)
+    };
+    if i == ~"0" {
+      LOW
+    } else {
+      HIGH
+    }
+  }
+}
+
+#[test]
+fn test_directions() {
+  let f = Pin::new(4);
+
+  f.set_direction(IN);
+  assert!(f.get_direction(), 1);
+
+  f.set_direction(OUT);
+  assert!(f.get_direction(), 0);
+
+  f.input();
+  assert!(f.get_direction(), IN);
+
+  f.output();
+  assert!(f.get_direction(), OUT);
+}
+
+#[test]
+fn test_values() {
+  let f = Pin::new(4);
+
+  f.set_value(HIGH);
+  assert!(f.get_value(), HIGH);
+
+  f.set_value(LOW);
+  assert!(f.get_value(), LOW);
+
+  f.high();
+  assert!(f.get_value(), HIGH);
+
+  f.low();
+  assert!(f.get_value(), LOW);
+}
+
+fn main() {
+  let f = Pin::new(4);
+
+  f.export();
+
+  f.set_direction(IN);
+  match f.get_direction() {
+    IN => println!("in"),
+    OUT => println!("out")
+  };
+
+  f.set_direction(OUT);
+  match f.get_direction() {
+    IN => println!("in"),
+    OUT => println!("out")
+  };
+
+  f.input();
+  match f.get_direction() {
+    IN => println!("in"),
+    OUT => println!("out")
+  };
+
+  f.output();
+  match f.get_direction() {
+    IN => println!("in"),
+    OUT => println!("out")
+  };
+
+  f.set_value(HIGH);
+  match f.get_value() {
+    HIGH => println!("high"),
+    LOW => println!("low")
+  };
+
+  f.set_value(LOW);
+  match f.get_value() {
+    HIGH => println!("high"),
+    LOW => println!("low")
+  };
+
+  f.high();
+  match f.get_value() {
+    HIGH => println!("high"),
+    LOW => println!("low")
+  };
+
+  f.low();
+  match f.get_value() {
+    HIGH => println!("high"),
+    LOW => println!("low")
+  };
+
+  f.unexport();
 }
